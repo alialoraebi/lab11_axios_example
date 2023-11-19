a4bc8cb20763d351f92467606be399c5c3186699
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2023-11-19 18:30:19 -0500

    import edited

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from "axios";
 import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
